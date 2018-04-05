b01cca8f7713f0421c6d81ec42d9cda07b2587bc
EdLo <b93303015@gmail.com>
2018-04-05 16:32:47 +0800

    新增 : 依照過濾條件抓取資料

--- crawl/http_crawl.py
+++ crawl/http_crawl.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import json
 
 url = 'https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2017/league/00_full_schedule_week.json';
 urls = [];
@@ -9,10 +10,24 @@ urls = [];
 #     urls.append(tmp_url)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
-html = requests.get(url, headers=headers)
+html = requests.get(url, headers=headers).content
 # html = requests.get(url)
-html.encoding = 'utf-8'
-print(html.json())
+# html.encoding = 'utf-8'
+JSON_Dict = json.loads(html)
+JSON_lscd = JSON_Dict['lscd']
+
+game_info = []
+
+for data in JSON_lscd:
+    JSON_mscd = data['mscd']
+    JSON_GAMES = JSON_mscd['g']
+
+    for game in JSON_GAMES:
+        g = game['gweek']
+        if g == 1:
+            game_info.append(game)
+
+print(game_info)
 
 # fp = open('tmp.txt', 'wb')
 # fp.write(html.content)
